c832cb0b5165ecefa915e5ca92741e9ad201f7d3
Long <longlong2000hcm@gmail.com>
2020-02-23 01:12:40 +0200

    add fake product database

--- components/product.js
+++ components/product.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const has = require('has-value');
 const router = express.Router();
+const fs = require('fs')
+
+const getJSON = (filePath) => {
+    file = fs.readFileSync(filePath);
+    data = JSON.parse(file);
+    return data.data;
+}
+
+var productsArray = getJSON("./data/products.json");
+
+router.get('/', (req, res) => { 
+    fs.writeFileSync("./data/products.json", JSON.stringify({data: productsArray}));
+    res.json(productsArray); 
+});
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
+const productComponent = require('./components/product.js');
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/hello', (req, res) => res.send('Hello GET World!'));
+
+app.use('/products', productComponent);
 
 app.listen(port, ()=>{
     console.log(`API listening on http://localhost:${port}\n`);
